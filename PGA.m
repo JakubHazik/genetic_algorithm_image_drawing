6a028be96a13a4c62946f692bc4e324a160c313e
JakubHazik-VC <hazik.jakub@gmail.com>
2019-11-27 09:45:18 +0100

    nieco

--- PGA.m
+++ PGA.m
@@ -1,5 +1,5 @@
-% clear;
-% close all;
+%  clear;
+%  close all;
 addpath('genetic_toolbox');
 
 global popSize
@@ -7,51 +7,53 @@ global space
 global amps
 global bestPopSelection
 global imageSizeX imageSizeY circlesNum
-
-% image params
-circlesNum = 70;
-imageSizeX = 150;
-imageSizeY = 150;
-maxCircleRadius = 40;
-maxColorVal = 50;
-% read img
-originalImg = imread('fotoCustom.jpg');
-originalImg = imresize(originalImg, [imageSizeY imageSizeX]);
-% figure;
-% imshow(originalImg);
-% title('Original image')
-
-% GA params
-saveRate = 100;
-islandsNum = 9;
-migrationRate = 200;
-iterationsNum = 1;
+% 
+% % image params
+% circlesNum = 100;
+% imageSizeX = 200;
+% imageSizeY = 200;
+% maxCircleRadius = 40;
+% maxColorVal = 50;
+% % read img
+% originalImg = imread('fotoCustom.jpg');
+% originalImg = imresize(originalImg, [imageSizeY imageSizeX]);
+% % figure;
+% % imshow(originalImg);
+% % title('Original image')
+% 
+% % GA params
+% saveRate = 100;
+% islandsNum = 9;
+% migrationRate = 200;
+% iterationsNum = 1;
 generationsNum=10000;        %pocet generacii/ pokusov
-popSize = 80;              %velkost populacie
-bestPopSelection=ones(1, 12); %vektor definujuci vyber najlepsich retazcov
-space = [
-    zeros(1, circlesNum),               zeros(1, circlesNum),               zeros(1, circlesNum),                   zeros(1, circlesNum);
-    ones(1, circlesNum) * imageSizeX,   ones(1, circlesNum) * imageSizeY,   ones(1, circlesNum) * maxCircleRadius   ones(1, circlesNum) * maxColorVal
-];
-amps=space(2,:) * 0.05;       %aditivna mutacia;
-course=zeros(islandsNum, generationsNum);    %inicializacia pola pre uchovanie priebehu, aby sa nemusela v kazdom kroku menit velkost vektora
-average = zeros(1, generationsNum);
-% %%
+% popSize = 80;              %velkost populacie
+% bestPopSelection=ones(1, 12); %vektor definujuci vyber najlepsich retazcov
+% space = [
+%     zeros(1, circlesNum),               zeros(1, circlesNum),               zeros(1, circlesNum),                   zeros(1, circlesNum);
+%     ones(1, circlesNum) * imageSizeX,   ones(1, circlesNum) * imageSizeY,   ones(1, circlesNum) * maxCircleRadius   ones(1, circlesNum) * maxColorVal
+% ];
+% amps = space(2,:) * 0.05;       %aditivna mutacia;
+% course=zeros(islandsNum, generationsNum);    %inicializacia pola pre uchovanie priebehu, aby sa nemusela v kazdom kroku menit velkost vektora
+% average = zeros(1, generationsNum);
+% % %%
+% 
+% % set output dir
+% output_dir = "savings/" + sprintf("%s", datetime('now'));
+% mkdir(output_dir);
 
-% set output dir
-output_dir = "savings/" + sprintf("%s", datetime('now'));
-mkdir(output_dir);
+start = 2505;
 
 for j=1:iterationsNum
     %vygenerovanie novej generacie
 %     pop=genrpop(popSize * islandsNum,space);
-    pop = generatePop(originalImg, popSize * islandsNum, circlesNum, space);
+%     pop = generatePop(originalImg, popSize * islandsNum, circlesNum, space);
     pop = change(pop, 2, space);
     
     %vytvorenie vektora s fitnesom(funkcnej hodnoty)d pre kazdy retazec
     fit = computeFitness(originalImg, pop, imageSizeX, imageSizeY, circlesNum);
     
-    for i=1:generationsNum  %kym nenajde minimum funkcie
+    for i=start:generationsNum  %kym nenajde minimum funkcie
         for island=1:islandsNum
             islandRange = (island - 1)* popSize +1: (island )* popSize;
             [islandPop, islandFit, islandBestGenFit] = processGeneration(originalImg, pop(islandRange, :), fit(islandRange));
--- plotResult.m
+++ plotResult.m
@@ -1,6 +1,12 @@
 % load('resultWorkspace500.mat');
 gen = pop(1,:);
 
+% close all;
+% figure;
+% img = drawGAImage(gen, imageSizeX, imageSizeY, circlesNum);
+% imshow(img)
+
+
 % compare images
 figure(20);
 subplot(1,3,1);
@@ -19,7 +25,7 @@ imshow(diff)
 % vykreslenie fitness
 figure(11);
 hold off;
-plot(1:generationsNum, course);
+plot(1:length(course(1,:)), course);
 legend
 hold on;
 xlabel('Pocet vyhodnoteni');
