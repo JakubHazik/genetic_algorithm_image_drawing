1d1b958a2891d0ac51bc6312b319327d118aede0
JakubHazik-VC <hazik.jakub@gmail.com>
2019-11-27 09:44:23 +0100

    first results

--- addCircle.m
+++ addCircle.m
@@ -1,5 +1,6 @@
 function [image] = addCircle(image, imageSizeX, imageSizeY, x_pos, y_pos, radius, color)
     w = 3.5;
+    w= 3;
     [columnsInImage, rowsInImage] = meshgrid(1:imageSizeX, 1:imageSizeY);
     circlePixels = ((rowsInImage - x_pos).^2 + (columnsInImage - y_pos).^2 <= radius^2) .* color;
     % imshow(circlePixels, [0 256]);
--- makeMigration.m
+++ makeMigration.m
@@ -1,6 +1,7 @@
 function [pop, fit] = makeMigration(pop, fit, islandsNum)
     global popSize
     
+    % hviezda
     % od 2 pretoze vsetky migrujeme do 1. ostrova
     for island = 2: islandsNum
         bestStringIndex = (island - 1) * popSize +1;
@@ -10,5 +11,19 @@ function [pop, fit] = makeMigration(pop, fit, islandsNum)
         pop(popSize - island + 2, :) = bestString;
         fit(popSize - island + 2) = bestStringFit;    
     end
+        
+    %uzavrety kruh bez 1
+    for island = 2: (islandsNum -1)
+        bestStringIndex = (island - 1) * popSize +1;
+        bestString = pop(bestStringIndex, :);
+        bestStringFit = fit(bestStringIndex);
+        pop(popSize * (island + 1), :) = bestString;
+        fit(popSize * (island + 1)) = bestStringFit;    
+    end
+    % este prvy s poslednym
+    bestStringIndex = (islandsNum - 1) * popSize + 1;
+    bestString = pop(bestStringIndex, :);
+    bestStringFit = fit(bestStringIndex);
+    pop(popSize * 2, :) = bestString;
+    fit(popSize * 2) = bestStringFit;
 end
-
